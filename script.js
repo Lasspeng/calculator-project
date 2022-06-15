8a79e47d910cc1621d23d236b9af8a83be9a1483
Gavin Liburd <GavinLiburd901@gmail.com>
2022-06-15 14:27:06 -0400

    Add functionality to the rest of the buttons and added display for results

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function operate(x, operator, y) {
     return add(x,y)
   } else if (operator === '-') {
     return subtract(x,y)
-  } else if (operator === 'x') {
+  } else if (operator === '*') {
     return multiply(x,y)
   } else if (operator === '÷') {
     return divide(x,y)
@@ -33,16 +33,27 @@ let secondOperand = "";
 let operator = "";
 let display = ""
 
+function reset () {
+  firstOperand = ""
+  secondOperand = ""
+  operator = ""
+  display = ""
+}
+
+const realDisplay = document.querySelector(".display");
+
 const numbers = document.getElementsByClassName("number");
 for (const button of numbers) {
   button.addEventListener("click", function() {
     if (operator === "") {
       firstOperand += button.innerHTML
       display = firstOperand
+      realDisplay.textContent = display;
       console.log(display)
     } else if (operator != "") {
       secondOperand += button.innerHTML
       display = `${firstOperand} ${operator} ${secondOperand}`
+      realDisplay.textContent = display;
       console.log(display)
     }
   })
@@ -53,13 +64,18 @@ for (const button of operators) {
   button.addEventListener("click", function() {
     operator = button.innerHTML;
     display = `${firstOperand} ${operator}`
+    realDisplay.textContent = display;
     console.log(display)
   })
 }
 
 const equals = document.querySelector(".equals");
 equals.addEventListener("click", function() {
+  if (operator === "") {
+    return 
+  }
   result = operate(firstOperand, operator, secondOperand);
+  realDisplay.textContent = result;
   firstOperand = result;
   secondOperand = "";
   console.log(result);
@@ -67,10 +83,30 @@ equals.addEventListener("click", function() {
 
 const posToNeg = document.querySelector(".positiveToNegative");
 posToNeg.addEventListener("click", function() {
-  -Math.abs(firstOperand);
-  if (display.includes("+" || "-" || "x" || "÷")) {
-    -Math.abs(secondOperand);
+  if (operator === "") {
+    firstOperand = "-" + firstOperand;
+    realDisplay.textContent = display;
+    console.log(display)
+  } else if (operator != "") {
+    secondOperand = "-" + secondOperand;
+    realDisplay.textContent = display;
+    console.log(display)
   }
-  console.log(display);
 })
 
+const clear = document.querySelector(".clear-all");
+clear.addEventListener("click", function() {
+  reset();
+  realDisplay.textContent = display;
+})
+
+const decimal = document.querySelector(".decimal");
+decimal.addEventListener("click", function() {
+  if (operator === "") {
+    firstOperand += decimal.innerHTML
+    realDisplay.textContent = display;
+  } else if (operator != "") {
+    secondOperand += decimal.innerHTML
+    realDisplay.textContent = display;
+  }
+})
